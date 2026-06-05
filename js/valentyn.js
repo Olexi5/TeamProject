7b6a4d8ab2150ce84915ec50c4f246aa403b6d3a
ValentynVarenyk <varenykvalentyn@gmail.com>
2026-06-05 17:36:58 +0300

    guess-number JS cimpleted

---
+++ js/valentyn.js
@@ -0,0 +1,15 @@
+// guess number
+// let randomNumber = Number(Math.round(Math.random() * (20 - 1) + 1));
+
+const guessNumberResult = document.querySelector(".guess-number-result");
+const guessNumberForm = document.querySelector(".guess-number-form");
+guessNumberForm.addEventListener("submit", (e) => {
+  const elements2 = e.currentTarget.elements;
+  e.preventDefault();
+  const randomNumber = Number(Math.round(Math.random() * (4 - 1) + 1));
+  if (elements2.Number123.value == randomNumber) {
+    guessNumberResult.textContent = `Вітаю, ви вгадали число! ${randomNumber}`;
+  } else {
+    guessNumberResult.textContent = `Ви програли, комп’ютер загадав ${randomNumber}`;
+  }
+});
